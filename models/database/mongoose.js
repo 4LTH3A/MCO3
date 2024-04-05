d3ba92fd54fe1a39f3818e424d28b6a716e9562f
Althea <reina_garcia@dlsu.edu.ph>
2024-04-05 20:17:13 +0800

    Update mongoose.js

--- models/database/mongoose.js
+++ models/database/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const run = async () => {
-  await mongoose.connect("mongodb://127.0.0.1:27017/myDB");
+  await mongoose.connect("mongodb://localhost:27017/Laundry_Shop");
   console.log("Connected to myDB");
 }
 
